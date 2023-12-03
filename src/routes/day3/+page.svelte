d075b73dc62fff8217c0776f5cabdaf001a4a0fa
Jesús Enrique Rascón <blograso@gmail.com>
2023-12-03 11:52:21 +0100

    add tweening

--- src/routes/day3/+page.svelte
+++ src/routes/day3/+page.svelte
@@ -5,14 +5,21 @@
 		isChecked: boolean;
 		id: number;
 	};
-
+	import { tweened } from 'svelte/motion';
+	import { backOut, elasticOut, quintInOut } from 'svelte/easing';
 	import BackButton from '$lib/Components/BackButton.svelte';
 	import Heading from '$lib/Components/Heading.svelte';
+
+	const weight = tweened(0, {
+		duration: 300,
+		easing: backOut
+	});
 
 	export let data;
 	let presents: Present[] = data.data;
 
 	$: selectedSum = presents.reduce((sum, item) => (item.isChecked ? sum + item.weight : sum), 0);
+	$: weight.set(selectedSum);
 	$: weightLeft = 100 - selectedSum;
 
 	function handleCheckboxChange(id: number) {
@@ -27,17 +34,21 @@
 
 <p>Let's pack this booga up.</p>
 
-<div class="mx-auto w-min flex flex-col space-y-3 my-10">
+<div class="mx-auto w-full flex flex-col space-y-3 my-10">
 	<div class="tabular-nums">
 		<span class="font-bold">{selectedSum.toFixed(2)}</span> kg out of 100 kg max.
 	</div>
-	<svg height="50px" class="my-10 border-2 border-dashed border-base-content rounded-xl">
-		<rect x="0" y="0" height="100px" width="{selectedSum}%" class="fill-primary" />
+	<svg
+		height="50px"
+		width="100%"
+		class="my-10 border-2 border-dashed border-base-content rounded-xl"
+	>
+		<rect x="0" y="0" height="100px" width="{$weight}%" class="fill-primary" />
 	</svg>
 
 	{#if presents}
 		<div class="border-2 border-dashed border-base-content rounded-xl h-[500px] overflow-y-scroll">
-			<table class="table table-lg font-normal mx-auto table-pin-rows">
+			<table class="table font-normal mx-auto table-pin-rows">
 				<thead>
 					<tr>
 						<th></th>
