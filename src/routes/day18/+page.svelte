5f4014706004e754266607dcd05b85e0fa4de81f
Jesús Enrique Rascón <blograso@gmail.com>
2023-12-18 22:52:54 +0100

    finish

--- src/routes/day18/+page.svelte
+++ src/routes/day18/+page.svelte
@@ -9,7 +9,9 @@
 
 	let answers: HTMLElement[] = [];
 	let userAnswers: boolean[] = [];
-	$: console.log(answers);
+	// from https://stackoverflow.com/questions/42317140/count-the-number-of-true-members-in-an-array-of-boolean-values
+	// wtf
+	$: totalCorrect = userAnswers.filter(Boolean).length;
 </script>
 
 <BackButton />
@@ -19,26 +21,29 @@
 <div>Let's test your knowledge! Santa has some questions to ask for you to answer.</div>
 
 <main class="my-10">
-	<div class="grid grid-cols-1 md:grid-cols-2 gap-10">
+	<div class="text-2xl mb-12">Correct answers: {totalCorrect} / {questions.length}</div>
+	<div class="grid grid-cols-1 md:grid-cols-2 gap-14 md:gap-10">
 		{#each questions as q, iq}
 			<div>
-				<div class="font-bold text-2xl">
+				<div class="font-bold text-2xl mb-3">
 					<span style="font-family: Fern Ornaments;" class="text-secondary">a</span>
 					{q.question}
 				</div>
 				<ol class="list-decimal list-inside tabular-nums text-xl">
 					{#each shuffle(q.answers) as a, ia}
-						<li bind:this={answers[iq * q.answers.length + ia]} class="flex items-center space-x-3">
+						<li bind:this={answers[iq * q.answers.length + ia]} class="flex items-start space-x-3">
 							<input
 								id="answer-{iq * q.answers.length + ia}"
 								disabled={userAnswers[iq] != undefined}
 								on:change={() => {
 									if (a.correct === true) {
 										userAnswers[iq] = true;
-										answers[iq * q.answers.length + ia].style.color = '#00FF00';
+										userAnswers = userAnswers;
+										answers[iq * q.answers.length + ia].style.color = '#9FE88D';
 									} else {
 										userAnswers[iq] = false;
-										answers[iq * q.answers.length + ia].style.color = '#FF0000';
+										userAnswers = userAnswers;
+										answers[iq * q.answers.length + ia].style.color = '#FF7D5C';
 									}
 								}}
 								class="checkbox pt-1"
